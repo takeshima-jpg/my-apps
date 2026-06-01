c7835d89b14ace1d50df4815d13fd85bee15566a
takeshima-jpg <takeshima-jpg@users.noreply.github.com>
2026-06-01 22:27:35 +0000

    shot-task-os: service-worker.jsのキャッシュを完全無効化

--- shot-task-os/service-worker.js
+++ shot-task-os/service-worker.js
@@ -1,27 +1,11 @@
-const CACHE_NAME = 'shot-task-os-v1';
-const ASSETS = [
-  './',
-  './index.html'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
+// キャッシュ無効化済み
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
-
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
